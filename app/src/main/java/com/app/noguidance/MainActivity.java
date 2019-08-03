253873a633fc5a7da4810fad5c654d2716ee981c
junecubes <junecubes@gmail.com>
2019-08-04 02:51:26 +0530

    9

--- app/src/main/java/com/app/noguidance/MainActivity.java
+++ app/src/main/java/com/app/noguidance/MainActivity.java
@@ -3,11 +3,21 @@ package com.app.noguidance;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+import com.startapp.android.publish.adsCommon.StartAppAd;
+import com.startapp.android.publish.adsCommon.StartAppSDK;
+
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        StartAppSDK.init(this, "207598944", true);
+    }
+
+    public void onBackPressed() {
+        StartAppAd.onBackPressed(this);
+        super.onBackPressed();
     }
 }
